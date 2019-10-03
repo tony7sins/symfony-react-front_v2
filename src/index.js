2691d8ef50151dad8bfde6b6506d69b7e8f91f8c
t7sg <tonigalczeskij@gmail.com>
2019-10-03 13:24:30 +0200

    5. Creating store, using Provider, Router and components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
+import { Router, Route, Switch } from 'react-router-dom'
+import history from './history'
+import App from './components/App';
+import LoginForm from './components/LoginForm'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const store = createStore(
+    state => state,
+    composeEnhancers(
+        applyMiddleware(reduxThunk)
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Fragment>
+                <h1>Header</h1>
+                <Switch>
+                    <Route path="/" exact component={App} />
+                    <Route path="/login" exact component={LoginForm} />
+                </Switch>
+            </Fragment>
+
+
+        </Router>
+    </Provider>
+    ,
+    document.getElementById('root')
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
